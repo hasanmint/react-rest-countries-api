6041dba58d8c94829a7d6447c7f9756e4ba8ae2f
hasanmint73 <hasanmint73@gmail.com>
2022-03-24 08:09:09 +0600

    Inline Commponent api data load

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { PureComponent, useEffect, useState } from 'react';
 
 function App() {
   return (
@@ -10,9 +11,37 @@ function App() {
 }
 
 function LoadCountries(){
+
+  const [countries ,setCountries]=useState([]);
+
+  useEffect( ()=>{
+    fetch('https://restcountries.com/v2/all')
+    .then(Response=>Response.json())
+    .then(data=>setCountries(data))
+  },[])
+
+
+
   return(
     <div>
       <h1>Visiting Every Countries</h1>
+      <h2>Avabilble Countries:{countries.length}</h2>
+      {
+        // countries.map(country=> <p>{country.name}</p>)//every country objects
+        countries.map(country=> <Country name={country.name} population={country.population}></Country>)//every country objects
+      }
+    </div>
+  )
+}
+
+
+function Country(props){
+  // console.log(props.name)
+  // console.log(props.population)
+  return(
+    <div>
+      <p><b>Name: </b>{props.name}</p>
+      <p><b>Population: </b>{props.population}</p>
     </div>
   )
 }
